206b756d608c791d0e037c60287c2a85e171d226
Chris Rhudy <chrisrhudy@gmail.com>
2017-01-28 01:33:00 -0600

    Add files via upload

--- stage2-project.py
+++ stage2-project.py
@@ -2,113 +2,111 @@
 
 #A FILL-IN-THE-BLANKS QUIZ for PYTHON
 
-
-#Lists of acceptable user-provided responses:
-
-affirmative_answers = ["yes", "Yes", "YES", "yes.", "Yes.", "YES.", "yes!", "Yes!", "YES!", "yep", "yup", "Yep", "Yup", "yep!", "Yep!", "YEP!", "Of course", "of course", "Of course!", "sure", "sure!", "Sure", "Sure!", "Sure.", "sure."]
-
-difficulty_levels = ["easy", "EASY", "Easy", "easy!", "Easy!", "EASY!", "easy.", "Easy.", "EASY.", "medium", "MEDIUM", "Medium", "medium.", "MEDIUM.", "Medium.", "medium!", "MEDIUM!", "Medium!", "hard", "Hard", "HARD", "hard.", "Hard.", "HARD.", "hard!", "Hard!", "HARD!"]
-
 #Three levels of quizzes, with four blanks in each:
-
 quiz_easy = """Hypertext Markup Language, or ___1___, is the basis for most documents found on the web. In ___1___,  each ___2___ is like a word in ___1___ language. Alternatively, ___2___s (such as <head> or <em>) can be looked at as elements in a tree. This tree-like structure of a page is also known as a ___3___ Object Model, or DOM. CSS (or ___4___ Style Sheets) allows you to use syntax and rules to change the look of an ___1___ page; in CSS, for example, including "border radius 50%" will change squares to circles!"""\
 
 quiz_medium = """An ___1___ is a conceptual idea, while a program is a concrete instantiation of an ___1___. Programming is the core of computer science. A ___2___ is simply a machine that can ___3___ (or "run") a program. With the right program, a ___2___ can do any mechanical computation you can imagine. A programming ___4___ is a ___4___ designed for producing ___2___ programs. Python is a programming ___4___."""\
 
 quiz_hard = """A procedure, also known in Python as a ___1___, enables you to abstract code from its inputs; an ___2___ statement allows you to write code that executes differently depending on the data, and is often paired with an ___3___ statement; and ___4___ loops (as well as for loops) provide a convenient way to repeat the same operations many times. Combining these procedures allows a programmer to, for example, solve the problem of finding all of the links on a web page!"""\
 
+#Lists of acceptable user-provided responses:
+affirmative_answers = ["yes", "Yes", "YES", "yes.", "Yes.", "YES.", "yes!", "Yes!", "YES!", "yep", "yup", "Yep", "Yup", "yep!", "Yep!", "YEP!", "Of course", "of course", "Of course!", "sure", "sure!", "Sure", "Sure!", "Sure.", "sure."]
+
+difficulty_levels = ["easy", "medium", "hard"]
+
 #Dictionaries for storing {blank:answers} pairs for each level of difficulty:
-
-dict_easy = {"___1___":("HTML", "html", "Html"), "___2___":("tag", "Tag", "TAG"), "___3___":("Document", "document", "DOCUMENT"), "___4___":("Cascading", "cascading", "CASCADING")}
-
-dict_medium = {"___1___":("algorithm", "Algorithm", "ALGORITHM"), "___2___":("computer", "Computer", "COMPUTER"), "___3___":("execute", "Execute", "EXECUTE"), "___4___":("language", "Language", "LANGUAGE")}
-
-dict_hard = {"___1___":("function", "Function", "FUNCTION"), "___2___":("if", "If", "IF"), "___3___":("else", "Else", "ELSE"), "___4___":("while", "While", "WHILE")}
+dict_easy = {"___1___":"html", "___2___":"tag", "___3___":"document", "___4___":"cascading"}
+dict_medium = {"___1___":"algorithm", "___2___":"computer", "___3___":"execute", "___4___":"language"}
+dict_hard = {"___1___":"function", "___2___":"if", "___3___":"else", "___4___":"while"}
 
 #Dictionary for storing player name, chosen level of difficulty, other info:
-dict_other = {"name":"", "quiz_level":"", "score":0, "spot":1, "blank":""}
-
+dict_other = {"name":"", "quiz_level":"", "score":0, "nth_blank":1, "total_blanks":4, "blank":"", "maximum_points":25, "minimum_points":10}
 
 #FUNCTIONS
 
 def game_intro():
-    #Serves as entry into the quiz; gets player's name, which is stored in dict_other
-    print "Welcome to ***PYTHON TRIVIA***  "
-    user_input = raw_input("What's your name?  ")
-    dict_other["name"] = user_input #Stores player's name in dict_other
-    print "Hello, " + dict_other["name"] + "!"
-    user_input = raw_input("Would you like to play now?  ")
-    if user_input in affirmative_answers:
+#   Behavior: Serves as entry into the quiz; gets player's name, which is stored in dict_other
+#   Inputs: User input re name and difficulty level
+#   Outputs: Explanatory text to user, determines difficulty level from user responses
+    print "Welcome to ***PYTHON TRIVIA*** "
+    user_name = raw_input("What's your name?  ")
+    dict_other["name"] = user_name #Stores player's name in dict_other
+    print "Hello, " + user_name + "!"
+    user_wants_to_play = raw_input("Would you like to play now?  ")
+    if user_wants_to_play in affirmative_answers:
         print "Great!"
     else:
         print "That's fine, too! Just input ctl-c at any time to exit." #Lets player exit from game at any time, but assumes the player actually does want to play
 
 def game_level():
-    #Prompts for and stores difficulty level in dict_other; explains scoring; leads into game itself
-    user_input = raw_input("Would you prefer an easy, medium, or hard level of difficulty?  ")
-    if user_input in difficulty_levels:
-        dict_other["quiz_level"] = user_input #Stores user's chosen level of difficulty
-        print "You have chosen the " + dict_other["quiz_level"] + " level, " + dict_other["name"] + ". Scoring works as follows: 2.5 points for a correct answer on the first try, 1 point for a correct answer on the second try, and 0 points otherwise. Good luck!"
+#   Behavior: Prompts for and stores difficulty level in dict_other; explains scoring; leads into game itself
+#   Inputs: Only user input to determine game difficulty level
+#   Outputs: Game explanation to user, plus determination of difficulty level
+    user_difficulty_choice = raw_input("Would you prefer an easy, medium, or hard level of difficulty?  ")
+    if user_difficulty_choice.lower() in difficulty_levels:
+        dict_other["quiz_level"] = user_difficulty_choice.lower() #Stores user's chosen level of difficulty
+        print "You have chosen the " + dict_other["quiz_level"] + " level, " + dict_other["name"] + ". Scoring works as follows: " + str(dict_other["maximum_points"]) + " points for a correct answer on the first try, " + str(dict_other["minimum_points"]) + " points for a correct answer on the second try, and 0 points otherwise. Good luck!"
     else:
-        user_input = raw_input("I'm sorry but I don't recognize that level--please choose your level again: type easy, medium, or hard. ")
-        if user_input in difficulty_levels:
-            dict_other["quiz_level"] = user_input #Stores player's chosen difficulty level in dict_other
-            print "You have chosen the " + dict_other["quiz_level"] + " level, " + dict_other["name"] + ". Scoring works as follows: 2.5 points for a correct answer on the first try, 1 point for a correct answer on the second try, and 0 points otherwise. Good luck!"
-        else:
-            print "That's fine, too! Just type ctl-c and <enter> to exit."
+        print "That's fine, too! Just type ctl-c and <enter> to exit."
+        dict_other["quiz_level"] = "easy"
     \
     print "Let's get started: press <enter> to see your fill-in-the-blanks quiz: "
-    user_input=raw_input()
+    pressed_enter=raw_input()
 
 def correct_answer(quiz, dic):
-    #Executes if user guesses right on first try
-    dict_other["score"] += 2.5
+#   Behavior: Judges first successful guesses for each blank
+#   Inputs: Takes quiz level and dictionary level, plus the user pressing <enter>
+#   Outputs: Outputs are scorekeeping and enthusiastic responses
+    dict_other["score"] += dict_other["maximum_points"]
     print "Correct! Your score is now: " + str(dict_other["score"]) + " point(s). Press <enter> to advance."
-    user_input = raw_input()
-    dict_other["spot"] += 1
+    pressed_enter = raw_input()
+    dict_other["nth_blank"] += 1
 
 def wrong_answer(quiz, dic):
-    #Executes if user guesses wrong on first try; provides one additional opportunity for user to guess right, updates score accordingly
-    user_input = raw_input("I'm sorry but that's incorrect, " + dict_other["name"] + ". Try again!  ")
-    if user_input in dic[dict_other["blank"]]: #Correct answer on second try
+#   Behavior: Judges second chance guesses for each blank
+#   Inputs: Takes quiz level and dictionary level, plus user input
+#   Outputs: Outputs are responses, judgments, and scorekeeping
+    next_guess = raw_input("I'm sorry but that's incorrect, " + dict_other["name"] + ". Try again!  ")
+    if next_guess.lower() == dic[dict_other["blank"]]: #Correct answer on second try
         print "Correct!"
-        dict_other["score"] += 1.0
+        dict_other["score"] += dict_other["minimum_points"]
         print "Your score is now: " + str(dict_other["score"]) + " point(s). Press <enter> to advance."
     else: #If two wrong answers, moves on to next q or end of game
-        if dict_other["spot"] == 4:
-            print "Wrong again! The correct answer is '" + dic[dict_other["blank"]][0] + ".' Press <enter>."
+        if dict_other["nth_blank"] == dict_other["total_blanks"]:
+            print "Wrong again! The correct answer is '" + dic[dict_other["blank"]] + ".' Press <enter>."
         else:
-            print "Wrong again! The correct answer is '" + dic[dict_other["blank"]][0] + ".' Let's try the next blank. Press <enter> to move on."
-    user_input = raw_input()
-    dict_other["spot"] += 1
+            print "Wrong again! The correct answer is '" + dic[dict_other["blank"]] + ".' Let's try the next blank. Press <enter> to move on."
+    pressed_enter = raw_input()
+    dict_other["nth_blank"] += 1
 
 def game_play(quiz, dic):
-    #The primary mechanism of question-and-answer. Takes user input from above functions, which is stored in the above dictionaries. Inputs required are the level of quiz and level of dictionary.
-    while dict_other["spot"] <= 4:
-        dict_other["blank"] = "___" + str(dict_other["spot"]) + "___" #To find each numbered blank
+#   Behavior: The primary mechanism of question-and-answer. Takes user input from above functions, which is stored in the above dictionaries. Inputs required are the level of quiz and level of dictionary.
+#   Inputs: Takes quiz level and dictionary level, as well as user input.
+#   Outputs: Outputs are responses, judgments, and scorekeeping
+    while dict_other["nth_blank"] <= dict_other["total_blanks"]:
+        dict_other["blank"] = "___" + str(dict_other["nth_blank"]) + "___" #To find each numbered blank
         if dict_other["blank"] in quiz:
             print quiz
-            user_input = raw_input("Please write your answer for blank " + str(dict_other["spot"]) + " here:  ")
-            if user_input in dic[dict_other["blank"]]: #For correct answer on first try
+            user_guess = raw_input("Please write your answer for blank " + str(dict_other["nth_blank"]) + " here:  ")
+            if user_guess.lower() == dic[dict_other["blank"]]: #For correct answer on first try
                 correct_answer(quiz, dic)
-                quiz = quiz.replace(dict_other["blank"], dic[dict_other["blank"]][0])#updates quiz
+                quiz = quiz.replace(dict_other["blank"], dic[dict_other["blank"]])#updates quiz
             else: #If wrong answer, player gets one more try
                 wrong_answer(quiz, dic)
-                quiz = quiz.replace(dict_other["blank"], dic[dict_other["blank"]][0])#updates quiz
+                quiz = quiz.replace(dict_other["blank"], dic[dict_other["blank"]])#updates quiz
     print "And that's it. Your final score is " + str(dict_other["score"]) + " point(s). Thanks for playing!"
 
 def the_complete_game():
-    #This combines the above functions and runs the whole game!
+#   Behavior: This combines the above functions and runs the whole game
+#   Inputs: Takes no inputs
+#   Outputs: Outputs are the responses, judgments, and scorekeeping for the whole game
     game_intro()
     game_level()
-    easy_level = difficulty_levels[:9] #Combines 9 possible user-provided "easy" values into one
-    medium_level = difficulty_levels[9:18] #Combines 9 possible "medium" values into one
-    hard_level =  difficulty_levels[18:27] #COmbines 9 user-provided "hard" values into one
-    if dict_other["quiz_level"] in easy_level:
+    if dict_other["quiz_level"].lower() == "easy":
         game_play(quiz_easy, dict_easy)
-    if dict_other["quiz_level"] in medium_level:
+    elif dict_other["quiz_level"].lower() == "medium":
         game_play(quiz_medium, dict_medium)
-    if dict_other["quiz_level"] in hard_level:
+    elif dict_other["quiz_level"].lower() == "hard":
         game_play(quiz_hard, dict_hard)
 
 the_complete_game()
